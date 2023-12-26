3ee565be7ecb57032779672ad6cff10be7e0c599
UmerJaved1997 <154904424+UmerJaved1997@users.noreply.github.com>
2023-12-26 19:57:04 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,86 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+import pandas as pd 
+import plotly.express as px
 
-"""
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+uploaded_file=st.file_uploader('Choose a CSV file containing only numerical values', 
+                 type='CSV', help='The file should contain only one column with no header', on_change=None)
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+if uploaded_file is not None:
+    dfExpModel = pd.read_csv(uploaded_file,header=None, names=['Temp'])
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+alpha=st.slider(
+    'Select value for Alpha',0.0,1.0,0.2,
+)
+
+st.write('')
+st.write('')
+
+
+
+beta=st.slider(
+    'Select value for Beta',0.0,1.0,0.3,
+)
+
+st.write('')
+st.write('')
+
+
+
+
+dfExpModel.insert(0, 'DayNumber', range(1, len(dfExpModel) + 1))
+
+dfHoltsMethod=dfExpModel.copy()
+
+dfExpModel['S(t)'] = dfExpModel['Temp'].copy()
+for i in range(1, len(dfExpModel)):
+    dfExpModel.at[i, 'S(t)'] = alpha * dfExpModel.at[i, 'Temp'] + (1-(alpha)) * dfExpModel.at[i - 1, 'S(t)']
+
+
+
+dfHoltsMethod['S(t)']=dfHoltsMethod['Temp'].copy()
+dfHoltsMethod['T(t)']=dfHoltsMethod['Temp'].copy()
+dfHoltsMethod['F(t)']=dfHoltsMethod['Temp'].copy()
+
+for i in range(0, len(dfHoltsMethod)):
+    if i==0:
+        dfHoltsMethod.at[i, 'S(t)'] = alpha * dfHoltsMethod.at[i, 'Temp'] 
+        dfHoltsMethod.at[i, 'T(t)'] = beta * dfHoltsMethod.at[i, 'S(t)'] 
+        dfHoltsMethod.at[i, 'F(t)']= None
+    else:
+        dfHoltsMethod.at[i, 'S(t)'] = (alpha * dfHoltsMethod.at[i, 'Temp']) + ((1-(alpha)) * (dfHoltsMethod.at[i - 1, 'S(t)']+dfHoltsMethod.at[i - 1, 'T(t)']))
+        dfHoltsMethod.at[i, 'T(t)'] = beta * ((dfHoltsMethod.at[i, 'S(t)'] - dfHoltsMethod.at[i-1, 'S(t)'])) + ((1-beta) * dfHoltsMethod.at[i - 1, 'T(t)']) 
+        dfHoltsMethod.at[i, 'F(t)']=  dfHoltsMethod.at[i-1, 'S(t)'] + dfHoltsMethod.at[i-1, 'T(t)']
+
+
+
+
+
+
+comparison_values = pd.concat([dfExpModel[['Temp','S(t)']], dfHoltsMethod['F(t)']], axis=1)
+
+comparison_values = comparison_values.rename(columns={'Temp': 'Actual', 'S(t)': 'ExpResult', 'F(t)': 'HoltResult'})
+
+comparison_values = comparison_values.drop(comparison_values.index[0]).reset_index(drop=True)
+
+
+
+
+Comparison_fig = px.line(comparison_values[['Actual','ExpResult','HoltResult']], 
+                title='Actual vs ExpResult vs HoltResult')
+st.plotly_chart(Comparison_fig, use_container_width=True)
+
+
+
+st.write('')
+st.write('Data of first 10 records when using Exponential smoothing model:')
+st.dataframe(dfExpModel.head(10))
+
+st.write('Data of first 10 records whne using Holt''s Method:')
+st.dataframe(dfHoltsMethod.head(10))
+
+
